8537d17b0d184a95c637bfbfe2540107c7aecca1
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-11 15:28:47 -0600

    Add changes up to 11:05 in Section 3

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { ScrollView, SafeAreaView } from "react-native";
 import styled from "styled-components";
 import Card from "./components/Card";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -13,6 +14,12 @@ export default function App() {
             <Avatar source={require("./assets/avatar.jpg")} />
             <Title>My first app!</Title>
             <Name>Meng</Name>
+            <Ionicons
+              name="ios-notifications"
+              size={32}
+              color="#4775f2"
+              style={{ position: "absolute", right: 20, top: 5 }}
+            />
             <StatusBar style="auto" />
           </TitleBar>
           <Subtitle>Continue Learning</Subtitle>
