f1775b133d05a973dd9e08cba0b55f7638846449
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-11 17:56:10 -0600

    Add changes up to 21:58 in Section 5

--- components/Menu.js
+++ components/Menu.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { Animated, LogBox, TouchableOpacity } from "react-native";
+import { Animated, LogBox, TouchableOpacity, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+
+const screenHeight = Dimensions.get("window").height;
 
 class Menu extends React.Component {
   state = {
-    top: new Animated.Value(900),
+    top: new Animated.Value(screenHeight),
   };
 
   componentDidMount() {
@@ -17,26 +19,57 @@ class Menu extends React.Component {
 
   toggleMenu = () => {
     Animated.spring(this.state.top, {
-      toValue: 900,
+      toValue: screenHeight,
     }).start();
   };
 
   render() {
     return (
       <AnimatedContainer style={{ top: this.state.top }}>
-        <Cover />
-        <TouchableOpacity onPress={this.toggleMenu}>
+        <Cover>
+          <Image source={require("../assets/background2.jpg")} />
+          <Title>Meng To</Title>
+          <Subtitle>meng@designcode.io</Subtitle>
+        </Cover>
+        <TouchableOpacity
+          onPress={this.toggleMenu}
+          style={{
+            position: "absolute",
+            top: 120,
+            left: "50%",
+            marginLeft: -22,
+            zIndex: 1,
+          }}
+        >
           <CloseView>
             <Ionicons name="ios-close" size={44} color="#546bfb" />
           </CloseView>
-          <Content />
         </TouchableOpacity>
+        <Content />
       </AnimatedContainer>
     );
   }
 }
 
 export default Menu;
+
+const Image = styled.Image`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 13px;
+  color: rgba(255, 255, 255, 0.5);
+  margin-top: 8px;
+`;
 
 const CloseView = styled.View`
   width: 44px;
@@ -45,6 +78,7 @@ const CloseView = styled.View`
   background: white;
   justify-content: center;
   align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
 `;
 
 const Container = styled.View`
@@ -60,9 +94,11 @@ const AnimatedContainer = Animated.createAnimatedComponent(Container);
 const Cover = styled.View`
   height: 142px;
   background: black;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Content = styled.View`
-  height: 900px;
+  height: ${screenHeight}px;
   background: #f0f3f5;
 `;
