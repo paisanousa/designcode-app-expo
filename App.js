c8df281e66a83593b40ec4a7902b0b064eb81220
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-12 07:06:43 -0600

    Add changes up to 4:05 in Section 8

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import "react-native-gesture-handler";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
+import AppNavigator from "./navigator/AppNavigator";
 
 const initialState = {
   action: "",
@@ -25,15 +22,10 @@ const reducer = (state = initialState, action) => {
 };
 
 const store = createStore(reducer);
-const Stack = createStackNavigator();
 
 const App = () => (
   <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppNavigator />
   </Provider>
 );
 
---
+++ navigator/AppNavigator.js
@@ -0,0 +1,17 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default AppNavigator;
