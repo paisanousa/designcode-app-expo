e39813dd6dba723c53be7002df764a6b7f26ae45
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-12 09:35:25 -0600

    Add changes up to 23:19 in Section 8

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,32 +1,90 @@
-// import "react-native-gesture-handler";
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from "@react-navigation/stack";
-// import HomeScreen from "../screens/HomeScreen";
-// import SectionScreen from "../screens/SectionScreen";
-import TabNavigator from "./TabNavigator";
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import SectionScreen from "../screens/SectionScreen";
+import { Ionicons } from "@expo/vector-icons";
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-// const AppNavigator = () => (
-//   <NavigationContainer>
-//     <Stack.Navigator initialRouteName="Home" mode="modal" headerMode="none">
-//       <Stack.Screen
-//         name="Home"
-//         component={HomeScreen}
-//         options={{
-//           title: "Home", // not necessary, name is default
-//         }}
-//       />
-//       <Stack.Screen
-//         name="Section"
-//         component={SectionScreen}
-//         options={{
-//           title: "Section", // not necessary, name is default
-//         }}
-//       />
-//     </Stack.Navigator>
-//   </NavigationContainer>
-// );
+const activeColor = "#4775f2";
+const inactiveColor = "#b8bece";
 
-export default TabNavigator;
+const CoursesStack = () => (
+  <Stack.Navigator headerMode="none">
+    <Stack.Screen
+      name="Courses"
+      component={SectionScreen}
+      options={{
+        title: "Courses", // not necessary, name is default
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const ProjectsStack = () => (
+  <Stack.Navigator headerMode="none">
+    <Stack.Screen
+      name="Projects"
+      component={SectionScreen}
+      options={{
+        title: "Projects", // not necessary, name is default
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const HomeTabs = () => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarLabel: "Home", // not necessary, name is default
+        tabBarIcon: ({ focused }) => (
+          <Ionicons name="ios-home" size={26} color={focused ? activeColor : inactiveColor} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Courses"
+      component={CoursesStack}
+      options={{
+        tabBarLabel: "Courses", // not necessary, name is default
+        tabBarIcon: ({ focused }) => (
+          <Ionicons name="ios-albums" size={26} color={focused ? activeColor : inactiveColor} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Projects"
+      component={ProjectsStack}
+      options={{
+        tabBarLabel: "Projects", // not necessary, name is default
+        tabBarIcon: ({ focused }) => (
+          <Ionicons name="ios-folder" size={26} color={focused ? activeColor : inactiveColor} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
+
+const AppNavigator = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home" mode="modal" headerMode="none">
+      <Stack.Screen name="Home" component={HomeTabs} />
+      <Stack.Screen
+        name="Section"
+        component={SectionScreen}
+        options={{
+          title: "Section", // not necessary, name is default
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default AppNavigator;
