627534ca94200072b6b7c04091b92aa477867306
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-11 20:19:34 -0600

    Add changes up to 10:22 in Section 7

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,6 +6,22 @@ class Avatar extends React.Component {
     photo: "https://dl.dropboxusercontent.com/s/zsa94qhrfm8yf5e/avatar-default.jpg?dl=0",
   };
 
+  componentDidMount() {
+    fetch("https://uifaces.co/api", {
+      method: "GET",
+      headers: {
+        "X-API-KEY": ["B4F2C0EB-172A425F-AEBC5BD9-2B3841F8"],
+        Accept: "application/json",
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        this.setState({ photo: response[0].photo });
+      });
+  }
+
   render() {
     return <Image source={{ uri: this.state.photo }} />;
   }
