d7ebaa7f5eea489536d35129532ff33c4e2bc22a
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-12 06:49:37 -0600

    Perform initial React Nav v5 integration

--- App.js
+++ App.js
@@ -1,75 +1,82 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { Button, LogBox, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-// import HomeScreen from "./screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-// const initialState = {
-//   action: "",
-//   name: "",
-// };
+const initialState = {
+  action: "",
+  name: "",
+};
 
-// const reducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "OPEN_MENU":
-//       return { action: "openMenu" };
-//     case "CLOSE_MENU":
-//       return { action: "closeMenu" };
-//     case "UPDATE_NAME":
-//       return { name: action.name };
-//     default:
-//       return state;
-//   }
-// };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return { action: "openMenu" };
+    case "CLOSE_MENU":
+      return { action: "closeMenu" };
+    case "UPDATE_NAME":
+      return { name: action.name };
+    default:
+      return state;
+  }
+};
 
-// const store = createStore(reducer);
-
-// const App = () => (
-//   <Provider store={store}>
-//     <HomeScreen />
-//   </Provider>
-// );
-
-// export default App;
-
-//===========================================================================
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Details" onPress={() => navigation.navigate("Details")} />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        // onPress={() => navigation.navigate('Details')}
-        onPress={() => navigation.push("Details")}
-      />
-    </View>
-  );
-}
-
+const store = createStore(reducer);
 const Stack = createStackNavigator();
 
-function App() {
-  return (
+const App = () => (
+  <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-}
+  </Provider>
+);
 
 export default App;
+
+//===========================================================================
+
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Details"
+//         onPress={() => navigation.navigate("Details")}
+//       />
+//     </View>
+//   );
+// }
+
+// function DetailsScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Details Screen</Text>
+//       <Button
+//         title="Go to Details... again"
+//         // onPress={() => navigation.navigate('Details')}
+//         onPress={() => navigation.push("Details")}
+//       />
+//     </View>
+//   );
+// }
+
+// const Stack = createStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Details" component={DetailsScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
