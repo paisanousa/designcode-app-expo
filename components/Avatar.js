dc9ef70024657cabb47e0790425be0cadd3d15f4
Dennis Sanders <dennissandersisi@yahoo.com>
2020-12-11 21:28:22 -0600

    Clean up console.log

--- components/Avatar.js
+++ components/Avatar.js
@@ -31,7 +31,7 @@ class Avatar extends React.Component {
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         this.setState({ photo: response[0].photo });
         this.props.updateName(response[0].name);
       });
